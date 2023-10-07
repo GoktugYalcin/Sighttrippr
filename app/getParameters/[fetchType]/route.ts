0d35f9db630d25c9849904b082c36a36231d3ff1
Goktug Yalcin <goktug.yalcin@next4biz.com>
2023-10-07 16:53:11 +0300

    [fix] Removed unnecessary comment

--- app/getParameters/[fetchType]/route.ts
+++ app/getParameters/[fetchType]/route.ts
@@ -2,10 +2,6 @@ import { NextResponse } from "next/server";
 import { Geonames } from "@/fetchers/generated/Geonames";
 import { Tripmap } from "@/fetchers/generated/Tripmap";
 import { Places } from "@/fetchers/generated/Places";
-/*
-https://maps.googleapis.com/maps/api/place/textsearch/json?query=tourist_attraction+name+includes+kasr%C4%B1&key=AIzaSyDrwmXyDHTbPZYzC_lR_V4FRY9H2OOaRKo&type=point_of_interest&location=41.01384,28.94966
-https://developers.google.com/maps/documentation/places/web-service/search-text?hl=tr
-*/
 export async function POST(
   request: Request,
   { params }: { params: { fetchType: string } },
